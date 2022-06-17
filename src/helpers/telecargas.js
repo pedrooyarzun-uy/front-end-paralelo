536735abee683b2971fc22a9d98e5ac4e14692f8
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2022-06-17 09:39:58 -0300

    Cambio v2 en telecargas

--- src/helpers/telecargas.js
+++ src/helpers/telecargas.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'localhost:9080/SAGAFrontEndServices/webapi',
+    baseURL: 'http://localhost:9080/SAGAFrontEndServices/webapi',
     headers: {'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
     }
